59a343055d33ca1324ef8b86ce4e4fa7a16c534b
JoakimChan <120197963+JoakimChan@users.noreply.github.com>
2024-01-30 17:55:03 +0100

    commit
    not tested

--- specs/bookingPrice.feature
+++ specs/bookingPrice.feature
@@ -9,20 +9,23 @@ Feature: 10. As a visitor, I want to be able to book tickets at different prices
     Given that the user at home page
     And choosing on a movie
     And time and date
+    And set total tickets to zero
     When ordering only "3" child tickets
-    Then the total cost should be "240" kr
+    Then the total cost should be equal to "240" kr
 
   Scenario: Booking tickets for seniors
     Given that the user at home page
     And choosing on a movie
     And time and date
+    And set total tickets to zero
     When ordering only "2" seniors tickets
-    Then the total cost should be "240" kr
+    Then the total cost should be equal to "240" kr
 
   Scenario: Booking tickets for mixed ages
     Given that the user at home page
     And choosing on a movie
     And time and date
+    And set total tickets to zero
     When ordering only "1" original ticket, "2" senior tickets and "3" children ticket
     Then the total cost should be equal to "620" kr
 
@@ -30,5 +33,5 @@ Feature: 10. As a visitor, I want to be able to book tickets at different prices
     Given that the user at home page
     And choosing on a movie
     And time and date
-    When ordering only "0" ticket
+    And set total tickets to zero
     Then the total cost should be equal to "0" kr
--- specs/step_definitions/bookingPrice.js
+++ specs/step_definitions/bookingPrice.js
@@ -12,34 +12,44 @@ Given('time and date', () => {
   cy.get('button.screening-btn.text-nowrap.btn').first().click({ force: true });
 });
 
-When('ordering only {string} child tickets', (a) => {
+Given('set total tickets to zero', () => {
   while (cy.get('span').eq(4) != 0) {
     cy.get('svg').eq(0).click({ force: true })
   }
-
-  for (let i = 0; i < a; i++) {
-    cy.get('svg').eq(5).click({ force: true })
+  while (cy.get('span').eq(5) != 0) {
+    cy.get('svg').eq(2).click({ force: true })
+  }
+  while (cy.get('span').eq(6) != 0) {
+    cy.get('svg').eq(4).click({ force: true })
   }
 });
 
-Then('the total cost should be {string} kr', (a) => {
-  // TODO: implement step
-});
-
-When('ordering only {string} seniors tickets', (a) => {
-  // TODO: implement step
-});
-
-
-When('ordering only {string} original ticket, {string} senior tickets and {string} children ticket', (a, b, c) => {
-  // TODO: implement step
+When('ordering only {string} child tickets', (a) => {
+  for (let i = 0; i < a; i++) {
+    cy.get('svg').eq(3).click({ force: true })
+  }
 });
 
 Then('the total cost should be equal to {string} kr', (a) => {
-  // TODO: implement step
+  let totalCost = cy.get('span').eq(2)
+
+  expect(a).to.equal(totalCost)
 });
 
+When('ordering only {string} seniors tickets', (a) => {
+  for (let i = 0; i < a; i++) {
+    cy.get('svg').eq(3).click({ force: true })
+  }
+});
 
-When('ordering only {string} ticket', (a) => {
-  // TODO: implement step
+When('ordering only {string} original ticket, {string} senior tickets and {string} children ticket', (a, b, c) => {
+  for (let i = 0; i < a; i++) {
+    cy.get('svg').eq(1).click({ force: true })
+  }
+  for (let i = 0; i < b; i++) {
+    cy.get('svg').eq(2).click({ force: true })
+  }
+  for (let i = 0; i < c; i++) {
+    cy.get('svg').eq(5).click({ force: true })
+  }
 });
