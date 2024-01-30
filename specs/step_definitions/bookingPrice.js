feae472bea6c1e9064a4da5e70398eca8808a2b7
JoakimChan <120197963+JoakimChan@users.noreply.github.com>
2024-01-31 00:33:10 +0100

    commit

--- specs/step_definitions/bookingPrice.js
+++ specs/step_definitions/bookingPrice.js
@@ -5,60 +5,57 @@ Given('that the user at home page', () => {
 });
 
 Given('choosing on a movie', () => {
-  cy.get('.img-wrapper').eq(3).click({ force: true }).wait(5000)
+  //
+  cy.get('.img-wrapper').eq(3).should('be.visible').click({ force: true })
 });
 
 Given('time and date', () => {
-  cy.get('button.screening-btn.text-nowrap.btn').should('be.visible').first().click({ force: true }).wait(5000)
-  cy.wait(5000)
+  cy.get('button.screening-btn.text-nowrap.btn').should('be.visible').first().click({ force: true })
 });
 
 Given('set total tickets to {string}', (a) => {
-  while (cy.get('span').eq(4) != a) {
-    cy.get('svg').eq(0).click({ force: true }).wait(5000)
+  for (let i = 0; i < 2; i++) {
+    cy.get('svg').eq(0).should('be.visible').click({ force: true })
   }
-  while (cy.get('span').eq(5) != a) {
-    cy.get('svg').eq(2).click({ force: true }).wait(5000)
+  /*
+  while (cy.get('span').eq(4).should('be.visible') != a) {
+    cy.get('svg').eq(0).should('be.visible').click({ force: true })
   }
-  while (cy.get('span').eq(6) != a) {
-    cy.get('svg').eq(4).click({ force: true }).wait(5000)
+  while (cy.get('span').eq(5).should('be.visible') != a) {
+    cy.get('svg').eq(2).should('be.visible').click({ force: true })
   }
-  cy.wait(5000)
+  while (cy.get('span').eq(6).should('be.visible') != a) {
+    cy.get('svg').eq(4).should('be.visible').click({ force: true })
+  }
+  */
 });
 
 When('ordering only {string} child tickets', (a) => {
   for (let i = 0; i < a; i++) {
-    cy.get('svg').eq(3).click({ force: true }).wait(5000)
+    cy.get('svg').eq(5).should('be.visible').click({ force: true })
   }
-  cy.wait(5000)
 });
 
 Then('the total cost should be equal to {string} kr', (a) => {
-  let totalCost = cy.get('span').eq(2)
+  let totalCost = cy.get('span').eq(2).children().should('be.visible')
 
-  expect(a).to.equal(totalCost)
-  cy.wait(5000)
-
+  expect(a).to.equal(parseInt(totalCost))
 });
 
 When('ordering only {string} seniors tickets', (a) => {
   for (let i = 0; i < a; i++) {
-    cy.get('svg').eq(3).click({ force: true }).wait(5000)
+    cy.get('svg').eq(3).should('be.visible').click({ force: true })
   }
-  cy.wait(5000)
-
 });
 
 When('ordering only {string} original tickets, {string} senior tickets and {string} children tickets', (a, b, c) => {
   for (let i = 0; i < a; i++) {
-    cy.get('svg').eq(1).click({ force: true }).wait(5000)
+    cy.get('svg').eq(1).should('be.visible').click({ force: true })
   }
   for (let i = 0; i < b; i++) {
-    cy.get('svg').eq(3).click({ force: true }).wait(5000)
+    cy.get('svg').eq(3).should('be.visible').click({ force: true })
   }
   for (let i = 0; i < c; i++) {
-    cy.get('svg').eq(5).click({ force: true }).wait(5000)
+    cy.get('svg').eq(5).should('be.visible').click({ force: true })
   }
-  cy.wait(5000)
-
 });
