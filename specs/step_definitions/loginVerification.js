4cb7b9abc8411d5d8f82f1fc82e164e4ea8db74a
Sykeet <146730918+Sykeet@users.noreply.github.com>
2024-01-31 12:33:42 +0100

    update minor fixes

--- specs/step_definitions/loginVerification.js
+++ specs/step_definitions/loginVerification.js
@@ -2,24 +2,24 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 //test-1
 
-Given("the user is on the 'Logga In' page", () => {
+Given('the user is on the "Logga In" page', () => {
   cy.visit('/logga-in');
 });
 
-When("the user enters their correct email and password", () => {
+When('the user enters their correct email and password', () => {
   cy.get('input[name="email"]').type('kedipi3878@konican.com');
   cy.get('input[name="password"]').type('ABc12345');
 });
 
-When("the user clicks the login button", () => {
+When('the user clicks the login button', () => {
   cy.get('.form-wrapper.container button[type="submit"]').click();
 });
 
-Then("the user should be redirected to the start page", () => {
+Then('the user should be redirected to the start page', () => {
   cy.url().should('eq', Cypress.config().baseUrl);
 });
 
-Then("the user should be logged in successfully", () => {
+Then('the user should be logged in successfully', () => {
   cy.visit('/');
   cy.get('a.nav-link.logout-btn').should('be.visible');
 });
@@ -64,11 +64,26 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   return true;
 });
 
-When('the user presses Min Sida', () => {
+When('the user presses "Min Sida"', () => {
   cy.get('a[href="/min-sida"]').should('be.visible').click();
 });
 
-Then('the site should navigate to Min Sida', () => {
+Then('the site should navigate to "Min Sida"', () => {
   cy.url().should('include', '/min-sida');
   cy.contains('h6', 'Dina uppgifter').should('be.visible');
+});
+
+//test-5
+
+When('the user clicks on "Redigera"', () => {
+  cy.contains('button', 'Redigera').should('be.visible').click();
+});
+
+Then('the user should be able to change their email, firstname, lastname and phonenumber', () => {
+  cy.get('input[name="email"]').should('be.visible').clear().type('newemail@example.com');
+  cy.get('input[name="firstname"]').should('be.visible').clear().type('NewFirstName');
+  cy.get('input[name="lastname"]').should('be.visible').clear().type('NewLastName');
+  cy.get('input[name="phone"]').should('be.visible').clear().type('1234567890');
+  //nedan utkommenterad för att den kommer ändra inlogg som används
+  //cy.get('form').submit();
 });
