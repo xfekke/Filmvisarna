76981215555881dd7f84ea56ea5289091b1681e5
xfekke <fekke4201337@gmail.com>
2024-02-06 23:48:52 +0100

    seatsAvailable
    Updated the feature and scenario to be a if else. Otherwise the test would've always failed with the old code.

--- specs/seatsAvailable.feature
+++ specs/seatsAvailable.feature
@@ -4,8 +4,3 @@ Feature: Showing available seats for a movie showing in the cinema
     Given that I have selected the movie and know what date I want to go
     When I click on “Välj plats” I should be taken to a view of all the seats
     Then I should be able to see a view of the available seats
-
-  Scenario: User wants to see what seats are available when none is
-    Given that I have selected the movie and know what date I want to go
-    When I click on “Välj plats” I should be taken to a view of all the seats
-    Then I should be able to see a view of a fully booked cinema
--- specs/step_definitions/seatsAvailable.js
+++ specs/step_definitions/seatsAvailable.js
@@ -3,30 +3,38 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 // Scenario 1: Users wants to see what seats are available
 
 Given('that I have selected the movie and know what date I want to go', () => {
-  // Goes to the Oppenheimer movie since it has the most showings
-  cy.visit('/film/3');
+  // Change movie here depending if you want to test a fully booked one or not
+  cy.visit('/film/1');
 });
 
 When('I click on “Välj plats” I should be taken to a view of all the seats', () => {
   // Looks for the "Välj plats" button and clicks it
   // then navigates to the grid of seats
-  // force to click element even when covered
   cy.get('button.screening-btn.text-nowrap.btn').first().click({ force: true });
 });
 
 Then('I should be able to see a view of the available seats', () => {
-  // Looks for available seats
-  // if any seat is available test goes through
-  // logs the amount of seats available
-  // looks for 2 available seats at minimum
-  cy.get('div.seat:not(.booked):not(.selected)').should('have.length.gte', 2);
-});
 
-// Scenario 2: User wants to see what seats are available when none is
-
-Then('I should be able to see a view of a fully booked cinema', () => {
-  // Checking if cinema is fully booked
-  // only checks the first 81 seats since the 3 last ones are examples only
+  // Website auto-chooses 2 seats, this is in considiration of the code
+  // Need wait for the website to load all the seats
   cy.wait(2000);
-  cy.get('div.seat:lt(81)').should('have.class', 'booked');
+  // Check if any seat is available
+  cy.get('div.seat:not(.booked):not(.selected)').then(($seats) => {
+
+    // Saves the first 81 seats since the last 3 can't be interacted with
+    const interactiveSeats = $seats.slice(0, 81);
+    // If loop to check length
+    if (interactiveSeats.length > 1) {
+      // If available seats are found, log the amount of seats available
+      cy.log(`There are ${interactiveSeats.length} available seats.`);
+
+      // Check for a minimum of 2 available seats
+      expect(interactiveSeats.length).to.be.gte(2);
+    } else {
+      // If all seats are booked-log
+      cy.log('All seats are booked.');
+      // Then the "boka" should be disabled
+      cy.get('button.btn.btn-custom').contains('Boka').should('have.attr', 'disabled');
+    }
+  });
 });
